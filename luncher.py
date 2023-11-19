d4e4cc77201ee43f38420773da2e1a1a71af175c
OtohaYuine <151360351+OtohaYuine@users.noreply.github.com>
2023-11-19 13:39:40 +0900

    Add files via upload

---
+++ luncher.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+root = tk.Tk()
+
+
+#----ランチャーのウィンドウサイズ、位置の調整----
+#モニターの縦、横のピクセル数を取得
+monitor_width = root.winfo_screenwidth()
+monitor_height = root.winfo_screenheight()
+print(monitor_width)
+print(monitor_height)
+
+#モニターのピクセル数÷3(小数点切り捨て)をし、ランチャーのウィンドウサイズを計算する
+window_width = monitor_width//3
+window_height = monitor_height//3
+print(window_width)
+print(window_height)
+
+#ランチャーのウィンドウサイズ、ウィンドウ位置を調整する
+windowsize = str(window_width) + "x" + str(window_height) + "+" + str(window_width) + "+" + str(window_height)
+print(windowsize)
+root.geometry(windowsize)
+
+#タイトルバーを非表示にする
+root.overrideredirect(True)
+
+
+root.mainloop()
